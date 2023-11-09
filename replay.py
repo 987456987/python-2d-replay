3ed8ba07d9ad20a9c007573a440a93d4bf97f419
987456987 <donufrak112697@gmail.com>
2023-11-08 22:48:33 -0700

    name tags

--- replay.py
+++ replay.py
@@ -3,6 +3,8 @@ import json
 
 import pygame_gui
 
+import pygame.font
+
 # Initialize Pygame
 pygame.init()
 
@@ -11,6 +13,9 @@ WIDTH, HEIGHT = 1024, 1024
 FPS = 200
 FAST_FORWARD_SPEED = 100  # Change this to your desired speed
 clock = pygame.time.Clock()
+
+# font and size
+font = pygame.font.Font(None, 16)
 
 # Load JSON data
 with open('round_data.json') as f:
@@ -25,6 +30,8 @@ map_image = pygame.image.load('de_ancient.png')
 ct_player_image = pygame.image.load('ctPlayer.png')
 t_player_image = pygame.image.load('tPlayer.png')
 player_image = ''
+
+currentRound = 4
 
 
 # Set the desired player image size
@@ -46,7 +53,7 @@ manager = pygame_gui.UIManager((WIDTH, HEIGHT))
 slider = pygame_gui.elements.UIHorizontalSlider(
     relative_rect=pygame.Rect((100, 100), (800, 20)),  # Position and size of the slider
     start_value=0,  # Initial value (0.0 to 1.0)
-    value_range=(0, len(data[8]['Tick']) - 1),  # Value range
+    value_range=(0, len(data[currentRound]['Tick']) - 1),  # Value range
     manager=manager,
 )
 
@@ -67,15 +74,15 @@ while running:
 
     if map_image and ct_player_image and t_player_image:
         for i in range(10):
-            if data[8]['Tick'][position]['PlayerPositions'][i]['IsAlive']:
-                if data[8]['Tick'][position]['PlayerPositions'][i]['Team'] == 2:
+            if data[currentRound]['Tick'][position]['PlayerPositions'][i]['IsAlive']:
+                if data[currentRound]['Tick'][position]['PlayerPositions'][i]['Team'] == 2:
                     player_image = t_player_image
                 else:
                     player_image = ct_player_image
                 # Get coordinates and draw player image with the specified size
                 player_position = transform_coordinates([
-                    data[8]['Tick'][position]['PlayerPositions'][i]['Position']['X'],
-                    data[8]['Tick'][position]['PlayerPositions'][i]['Position']['Y']
+                    data[currentRound]['Tick'][position]['PlayerPositions'][i]['Position']['X'],
+                    data[currentRound]['Tick'][position]['PlayerPositions'][i]['Position']['Y']
                 ])
                 
 
@@ -87,7 +94,7 @@ while running:
                 scaled_player_image = pygame.transform.smoothscale(player_image, player_image_size)
 
                 # Calculate the angle of rotation (in degrees)
-                rotation_angle = data[8]['Tick'][position]['PlayerPositions'][i]['Rotation']
+                rotation_angle = data[currentRound]['Tick'][position]['PlayerPositions'][i]['Rotation']
 
                 # Rotate the player image
                 rotated_player_image = pygame.transform.rotate(scaled_player_image, rotation_angle)
@@ -100,6 +107,24 @@ while running:
 
                 # Blit the rotated image onto the screen
                 screen.blit(rotated_player_image, rotated_player_rect.topleft)
+                
+               # Render player's name above their head
+                player_name = data[currentRound]['Tick'][position]['PlayerPositions'][i]['Name']
+                text_surface = font.render(player_name, True, (255, 255, 255))  # Color: white
+                
+                background_color = (0, 0, 0, 128)  # Change the alpha value to adjust transparency
+                background_surface = pygame.Surface((text_surface.get_width(), text_surface.get_height()), pygame.SRCALPHA)
+                background_surface.fill(background_color)
+
+                # Calculate the position for the text and background
+                text_rect = text_surface.get_rect()
+                text_background_rect = background_surface.get_rect()
+                text_rect.midtop = (player_position[0] + player_image_size[0] // 2, player_position[1] - 12)
+                text_background_rect.midtop = text_rect.midtop
+
+                # Blit the background and then the text onto the screen
+                screen.blit(background_surface, text_background_rect)
+                screen.blit(text_surface, text_rect)
 
     # Event handling
     for event in pygame.event.get():
